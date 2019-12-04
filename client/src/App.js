7eead0dc7364e9d2c49aa9f163963774b9fa9949
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-12-03 21:17:51 -0600

    Created react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from "axios";
+
+const api = "http://localhost:9000/api/posts"
+
 function App() {
+
+  const [posts, setPosts] = useState([]);
+  const [change, setChange] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(api)
+      .then(response => {
+        console.log("GET response", response);
+        setPosts(response.data);
+      })
+  }, [change])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts.map(post => (
+        <div className="blog-post" key={post.id}>
+        <h2>{post.title}</h2>
+        <p>{post.contents}</p>
+        </div>
+      )
+      )}
+
+
     </div>
   );
 }
